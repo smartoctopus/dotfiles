2e502bb0942f907da0ef32ef750b21f5fcbfeb9d
smartoctopus <alessandro.bozzato.2007@outlook.it>
2022-07-10 15:43:32 +0200

    emacs: Change theme to modus theme

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -52,10 +52,30 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 
+;; Modus theme
+(setq modus-themes-mode-line '(accented borderless)
+      modus-themes-bold-constructs t
+      modus-themes-italic-constructs t
+      modus-themes-fringes 'subtle
+      modus-themes-tabs-accented t
+      modus-themes-paren-match '(bold intense)
+      modus-themes-prompts '(bold intense)
+      modus-themes-completions 'opinionated
+      modus-themes-org-blocks 'tinted-background
+      modus-themes-scale-headings t
+      modus-themes-region '(bg-only)
+      modus-themes-headings
+      '((1 . (rainbow overline background 1.4))
+        (2 . (rainbow background 1.3))
+        (3 . (rainbow bold 1.2))
+        (t . (semilight 1.1))))
+
+;; (load-theme 'modus-operandi t)
+(load-theme 'modus-vivendi t)
+
 ;; (setq doom-theme 'doom-one)
-;; (setq doom-theme 'modus-vivendi)
 ;; (setq doom-theme 'nord)
-(setq doom-theme 'gruvbox-dark-hard)
+;; (setq doom-theme 'gruvbox-dark-hard)
 
 ;; Set custom keybindings
 (map! :leader
