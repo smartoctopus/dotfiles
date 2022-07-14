deb6a082b91c29b556f6e981c1430a891cdd3942
Alessandro Bozzato <alessandro.bozzato.2007@outlook.it>
2022-07-14 20:54:34 +0200

    setup.sh: setup pinentry for emacs + gpg

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install deps (these also include emacs deps - sorry)
+sudo apt update && sudo apt install -y git libgpg-error-dev libassuan-dev lbzip2 autoconf automake autotools-dev bsd-mailx build-essential diffstat gnutls-dev imagemagick libasound2-dev libc6-dev libdatrie-dev libdbus-1-dev libgconf2-dev libgif-dev libgnutls28-dev libgpm-dev libgtk2.0-dev libgtk-3-dev libice-dev libjpeg-dev liblockfile-dev liblqr-1-0 libm17n-dev libmagickwand-dev libncurses5-dev libncurses-dev libotf-dev libpng-dev librsvg2-dev libsm-dev libthai-dev libtiff5-dev libtiff-dev libtinfo-dev libtool  libx11-dev libxext-dev libxi-dev libxml2-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxt-dev libxtst-dev libxv-dev quilt sharutils texinfo xaw3dg xaw3dg-dev xorg-dev xutils-dev zlib1g-dev libjansson-dev libxaw7-dev libselinux1-dev libmagick++-dev libacl1-dev
+
+# build pinentry-emacs
+cd ~
+wget https://gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2
+tar -xf pinentry-1.1.0.tar.bz2
+cd pinentry-1.1.0
+./configure --enable-pinentry-emacs --enable-inside-emacs
+make
+sudo make install
+cd ~
+rm -rf pinentry-1.1.0 pinentry-1.1.0.tar.bz2
+
+echo "pinentry-program /usr/local/bin/pinentry-emacs" > ~/.gnupg/gpg-agent.conf
