ee7f4b31b3e49dbbeb72451bad18de73456e9089
Alessandro Bozzato <alessandro.bozzato.2007@outlook.it>
2022-08-26 12:14:23 +0200

    setup.sh: THe script now needs to execute as root
    Bash creates a subshell every time you run a script. Inside this
    subshell you cannot input anything, so we cannot use sudo.
    We now request to execute the whole script as root to fix this issue.

--- setup.sh
+++ setup.sh
@@ -2,34 +2,47 @@
 
 set -e
 
-# Install emacs
-sudo add-apt-repository ppa:kelleyk/emacs
-sudo apt update
-sudo apt install emacs28-nativecomp
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+# Update repos
+add-apt-repository ppa:kelleyk/emacs
+apt update
+
+# Install emacs with native compilation
+apt install emacs28-nativecomp
 
 # Install doom emacs
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+git clone --depth 1 https://github.com/doomemacs/doomemacs /home/$real_user/.emacs.d
+/home/$real_user/.emacs.d/bin/doom install
 
 # Copy the configurations
-cp -r doom-emacs/* ~/.doom.d/
-cp .bash* ~
+cp -r doom-emacs/* /home/$real_user/.doom.d/
+cp .bash* /home/$real_user/
 
 git submodule update --init
 gpd-peda-pwndbg-gef/install.sh
 
 # Install deps (these also include emacs deps - sorry)
-sudo apt update && sudo apt install -y git libgpg-error-dev libassuan-dev lbzip2 autoconf automake autotools-dev bsd-mailx build-essential diffstat gnutls-dev imagemagick libasound2-dev libc6-dev libdatrie-dev libdbus-1-dev libgconf2-dev libgif-dev libgnutls28-dev libgpm-dev libgtk2.0-dev libgtk-3-dev libice-dev libjpeg-dev liblockfile-dev liblqr-1-0 libm17n-dev libmagickwand-dev libncurses5-dev libncurses-dev libotf-dev libpng-dev librsvg2-dev libsm-dev libthai-dev libtiff5-dev libtiff-dev libtinfo-dev libtool  libx11-dev libxext-dev libxi-dev libxml2-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxt-dev libxtst-dev libxv-dev quilt sharutils texinfo xaw3dg xaw3dg-dev xorg-dev xutils-dev zlib1g-dev libjansson-dev libxaw7-dev libselinux1-dev libmagick++-dev libacl1-dev
+apt install -y git libgpg-error-dev libassuan-dev lbzip2 autoconf automake autotools-dev bsd-mailx build-essential diffstat gnutls-dev imagemagick libasound2-dev libc6-dev libdatrie-dev libdbus-1-dev libgconf2-dev libgif-dev libgnutls28-dev libgpm-dev libgtk2.0-dev libgtk-3-dev libice-dev libjpeg-dev liblockfile-dev liblqr-1-0 libm17n-dev libmagickwand-dev libncurses5-dev libncurses-dev libotf-dev libpng-dev librsvg2-dev libsm-dev libthai-dev libtiff5-dev libtiff-dev libtinfo-dev libtool  libx11-dev libxext-dev libxi-dev libxml2-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxt-dev libxtst-dev libxv-dev quilt sharutils texinfo xaw3dg xaw3dg-dev xorg-dev xutils-dev zlib1g-dev libjansson-dev libxaw7-dev libselinux1-dev libmagick++-dev libacl1-dev
 
 # build pinentry-emacs
-cd ~
+cd /home/$real_user
 wget https://gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2
 tar -xf pinentry-1.1.0.tar.bz2
 cd pinentry-1.1.0
 ./configure --enable-pinentry-emacs --enable-inside-emacs
 make
-sudo make install
-cd ~
+make install
+cd /home/$real_user
 rm -rf pinentry-1.1.0 pinentry-1.1.0.tar.bz2
 
 echo "pinentry-program /usr/local/bin/pinentry-emacs" > ~/.gnupg/gpg-agent.conf
